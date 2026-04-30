9ce604bd9a1a0fec49fd2de38badb0586189e6ec
Yassir200 <yassiraitichou6@gmail.com>
2026-04-30 23:28:43 +0100

    Fix axios baseURL for production

--- frontend-budget/src/services/api.js
+++ frontend-budget/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // configuration d'axios
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', 
+  baseURL: '/api', 
 });
 
 // intercepteur pour attacher le token JWT aux requetes
